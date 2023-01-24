3c1e21cefbdf2d8944a65fe166b22d43e65c79c9
pastmax4 <46377444+pastmax4@users.noreply.github.com>
2023-01-24 13:55:43 +0100

    Update neural01NetLib.py

--- src/mieLibrerie/neural01NetLib.py
+++ src/mieLibrerie/neural01NetLib.py
@@ -70,6 +70,23 @@ class Network(object):
         self.sizes = sizes
         self.biases  = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def feedforward(self, a):
+        """Return the output of the network if ``a`` is input."""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+        return a
+
         
 
 
+#--------------------------------------------------------------------
+#### Miscellaneous functions
+#--------------------------------------------------------------------
+def sigmoid(z):
+    """The sigmoid function."""
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
